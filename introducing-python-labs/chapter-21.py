6157cce2cef5802df74b56c995e34570ec76be09
k041k4 <k041k4@gmail.com>
2023-01-02 17:03:16 -0800

    final

---
+++ introducing-python-labs/chapter-21.py
@@ -0,0 +1,11 @@
+#geospatila data platform
+import geopandas
+import matplotlib.pyplot as plt
+
+world_file = geopandas.datasets.get_path('naturalearth_lowres')
+world = geopandas.read_file(world_file)
+cities_file = geopandas.datasets.get_path('naturalearth_cities')
+cities = geopandas.read_file(cities_file)
+base = world.plot(color='orchid')
+cities.plot(ax=base, color='black', markersize=2)
+plt.show()
