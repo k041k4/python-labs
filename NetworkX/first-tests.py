345ee170899664943f26919a12be1c3320a6b884
k041k4 <k041k4@gmail.com>
2023-01-26 17:16:07 -0800

    nx

--- NetworkX/first-tests.py
+++ NetworkX/first-tests.py
@@ -1,6 +1,7 @@
 #
 import networkx as nx
-from matplotlib import pyplot as plt
+##from matplotlib import pyplot as plt
+from pyvis.network import Network
 
 G = nx.Graph()
 G.add_node('A')
@@ -8,6 +9,10 @@ G.add_nodes_from(['B', 'C'])
 G.add_edge('A', 'B')
 G.add_edges_from([('B', 'C'), ('A', 'C')])
 
-plt.figure(figsize=(6, 6))
+#plt.figure(figsize=(6, 6))
 nx.draw_networkx(G)
-plt.show()
+#plt.show()
+
+nt = Network('500px', '500px')
+nt.from_nx(G)
+nt.show('nx_simple.html')
