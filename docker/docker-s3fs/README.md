42910754c82d817d4e85e8ce52308edf5e71d7a0
vbaranek <vbaranek@amazon.com>
2024-04-12 09:31:12 -0700

    up

--- docker/docker-s3fs/README.md
+++ docker/docker-s3fs/README.md
@@ -1,11 +1,17 @@
 # Build the image
 
 ```dockerfile
-docker build . -t spheres_init --build-arg BUCKET_NAME=<s3_bucket_name>
+docker build . -t spheres_init
 ```
 
-# run container
+# run container structure
 
 ```dockerfile
-docker run -it -e ACCESS_KEY_ID=<access_key_id> -e SECRET_ACCESS_KEY=<secret_access_key> --privileged <image_tag>
+docker run -it -e ACCESS_KEY_ID=<access_key_id> -e SECRET_ACCESS_KEY=<secret_access_key> -e S3_ENDPOINT=<s3_endpoint> -e BUCKET_NAME=<bucket_name> --privileged <image_tag>
+```
+
+# run container example
+
+```dockerfile
+docker run -it -e ACCESS_KEY_ID=AKIA2IDIBYLUBFBMCO5A -e SECRET_ACCESS_KEY=O7x3Gl3UwGl90xqYxaTy4W1qG1YLoV0sgAl10g0V -e S3_ENDPOINT=https://s3.us-east-1.amazonaws.com -e BUCKET_NAME=us-east-1-vbaranek-spheres-containers --privileged spheres_init:latest
 ```
--- docker/docker-s3fs/deploy/main.py
+++ docker/docker-s3fs/deploy/main.py
@@ -7,4 +7,17 @@ if __name__ == "__main__":
     else:
         print(f"bucket not mounted :(")
     print("list s3 directories")
-    print(os.listdir(f"s3_bucket/"))
+    print(os.listdir(f"/home/spheres/s3_bucket/"))
+
+    print("STARTING DOCKER FINISHED BUILD")
+    os.system('docker build . -t hello:latest')
+
+    print("FINISHED BUILD")
+    os.system('docker images')
+
+    print("PUSHING IMAGE TO ECR")
+    os.system('aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 704592855784.dkr.ecr.us-east-1.amazonaws.com/us-east-1-vbaranek')
+    os.system('docker tag hello:latest 704592855784.dkr.ecr.us-east-1.amazonaws.com/us-east-1-vbaranek:hello')
+    os.system('docker push 704592855784.dkr.ecr.us-east-1.amazonaws.com/us-east-1-vbaranek:hello')
+
+    print("DONE")
